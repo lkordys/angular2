31515c6ca63313c1a4268a4d5b9129c1785b4e66
Lukasz <luk.kordys@gmail.com>
2017-09-18 22:01:20 +0200

    zalety i wady angulara

--- src/app/benefits/benefits.component.ts
+++ src/app/benefits/benefits.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenefitsComponent implements OnInit {
 
-  constructor() { }
+  advantagesList : String[];
+  disAdvantagesList : String[];
+
+  constructor() {
+    this.advantagesList = [
+      'TypeScript - możliwa kompilacja kodu w trakcie, statyczne typowanie, wsparcie dla IDE,',
+      'kompatybilność aplikacji zależna od dostawcy przeglądarki,',
+      'Single Page Application (SPA),',
+      'niewielkie wymagania technologiczne do uruchomienia aplikacji (nodeJS),',
+      'Angular CLI - jedna komenda do utworzenia działającego projektu',
+      'powiązania dwustronne (two-way-binding)',
+      'dostępność oraz redukcja kodu, czyli Ahead-of-Time (AOT)',
+      'wbudowane dyrektywy oraz możliwość pisania własnych',
+      'struktura aplikacji typu MVC,',
+      'możliwosć używania niezależnych modułów angularów,',
+      'wstrzykiwanie zależności (Dependency Injection)'];
+
+    this.disAdvantagesList = [
+      'ładowanie całości podczas odpalenia aplikacji,',
+      'zależność od backendu (REST kontroler),',
+      'złożony framework do nauczenia,',
+      'debugowanie kodu aplikacji,',
+      'asynchronicznie wykonywane funkcje aplikacji,',
+      'zbyt wiele $scope, $watch powoduje błędy na stronie',
+      'słabe wsparcie dla SEO',
+      'dość wolne w przypadku wyświetlania dużych ilości danych'
+    ]
+  }
 
   ngOnInit() {
   }
